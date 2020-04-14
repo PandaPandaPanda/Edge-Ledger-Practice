32cad1d82b780f787eda18a64e54095f8e9cbb3b
PandaPandaPanda <34975856+PandaPandaPanda@users.noreply.github.com>
2020-04-13 21:24:37 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Google Map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.querySelector('.map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    this.document.querySelector('#navbar').style.opacity = 1;
+}});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 65
+      },
+      800
+    );
+  }
+});
+
